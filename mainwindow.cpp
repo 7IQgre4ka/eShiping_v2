d841546ba0d8eadb0f3a32d4985853b0364c7054
7IQgre4ka <stahukmax1981@gmail.com>
2020-11-14 23:45:18 +0200

    UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -13,3 +11,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_cargoType_activated(const QString &arg1)
+{
+    QString inpurType = qs.toUtf8().constData();
+}
